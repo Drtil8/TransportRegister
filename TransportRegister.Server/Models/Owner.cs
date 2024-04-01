a6e2089746fe8652ac9c8951e5e107295f0c5f5d
David Drtil <david.drtil8@gmail.com>
2024-04-01 21:02:23 +0200

    Added user model

---
+++ TransportRegister.Server/Models/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace TransportRegister.Server.Models
+{
+    public class Owner
+    {
+        public Guid OwnerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- TransportRegister.Server/Models/Vehicle.cs
+++ TransportRegister.Server/Models/Vehicle.cs
@@ -2,7 +2,7 @@ namespace TransportRegister.Server.Models;
 
 public class Vehicle
 {
-    public Guid VehicleID { get; set; }
+    public Guid VehicleId { get; set; }
     public string Make { get; set; }
     public string Model { get; set; }
     public int Year { get; set; }
