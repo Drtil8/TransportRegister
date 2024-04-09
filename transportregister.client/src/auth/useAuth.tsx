0a0fe324cf61c37c37f76e6b3c602990703a46ae
David Drtil <david.drtil8@gmail.com>
2024-04-09 18:36:16 +0200

    Client - Moved files to auth dir

---
+++ transportregister.client/src/auth/useAuth.tsx
@@ -0,0 +1,11 @@
+﻿import { useContext } from 'react';
+import AuthContext from './AuthContext';
+
+// Hook (useX) for using the AuthContext
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === null) {
+    throw new Error("Hook useAuth must be used inside AuthProvider");
+  }
+  return context;
+};
