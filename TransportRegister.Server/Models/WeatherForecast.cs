d81ded4f63a3fa45b903cd61c111227ee5b76463
David Drtil <david.drtil8@gmail.com>
2024-04-09 18:49:24 +0200

    Removed nullable string after disable nullable option setting for project

--- TransportRegister.Server/Models/WeatherForecast.cs
+++ TransportRegister.Server/Models/WeatherForecast.cs
@@ -8,6 +8,6 @@ namespace TransportRegister.Server.Models
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
