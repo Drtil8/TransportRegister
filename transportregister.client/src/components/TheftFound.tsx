471d2b289afdbab2376d89a7c2679325dc291fca
xhosad00 <80786156+xhosad00@users.noreply.github.com>
2024-04-19 13:54:38 +0200

    Basic components (Driver, Offense, Theft, Vehicle)

---
+++ transportregister.client/src/components/TheftFound.tsx
@@ -0,0 +1,26 @@
+﻿import { Component } from 'react';
+import { Col, Row } from 'reactstrap';
+export class TheftFound extends Component<object> {
+  constructor(props: object) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col>
+            <h4>Nalezení vozidla</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Tabulka kradených a select nebo vyhledávání? + info o nalezení
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default TheftFound;
---
+++ transportregister.client/src/components/VehicelCreate.tsx
@@ -0,0 +1,26 @@
+﻿import { Component } from 'react';
+import { Col, Row } from 'reactstrap';
+export class VehicleCreate extends Component<object> {
+  constructor(props: object) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col>
+            <h4>Zaregistrovat vozidlo</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            View/form pro vozidla
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default VehicleCreate;
