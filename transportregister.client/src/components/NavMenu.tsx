a60f1e882208e8ac9e69b1dededd322113bcb446
xhosad00 <80786156+xhosad00@users.noreply.github.com>
2024-04-19 13:53:45 +0200

    Navbar_First

--- transportregister.client/src/components/NavMenu.tsx
+++ transportregister.client/src/components/NavMenu.tsx
@@ -1,9 +1,19 @@
-﻿import {Component, ContextType} from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+﻿import { Component, ContextType } from 'react';
+import { Collapse, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import AuthContext from '../auth/AuthContext';
 
-export class NavMenu extends Component<object, { collapsed: boolean }> {
+
+// TODO navbar when NavbarToggle - page is small (in hamburger menu)
+interface NavMenuState {
+  collapsed: boolean;
+  driverDropOpen: boolean;
+  vehicleDropOpen: boolean;
+  offenceDropOpen: boolean;
+  theftDropOpen: boolean;
+}
+
+export class NavMenu extends Component<object, NavMenuState> {
   static displayName = NavMenu.name;
   static contextType = AuthContext;
   declare context: ContextType<typeof AuthContext>;
@@ -13,7 +23,11 @@ export class NavMenu extends Component<object, { collapsed: boolean }> {
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true
+      collapsed: true,
+      driverDropOpen: false,
+      vehicleDropOpen: false,
+      offenceDropOpen: false,
+      theftDropOpen: false,
     };
   }
 
@@ -23,11 +37,41 @@ export class NavMenu extends Component<object, { collapsed: boolean }> {
     });
   }
 
+  toggleDriverDropdown = () => {
+    this.setState((prevState: NavMenuState) => ({
+      driverDropOpen: !prevState.driverDropOpen,
+    }));
+  };
+
+  toggleVehicleDropdown = () => {
+    this.setState((prevState: NavMenuState) => ({
+      vehicleDropOpen: !prevState.vehicleDropOpen,
+    }));
+  };
+
+  toggleOffenceDropdown = () => {
+    this.setState((prevState: NavMenuState) => ({
+      offenceDropOpen: !prevState.offenceDropOpen,
+    }));
+  };
+
+  toggleTheftDropdown = () => {
+    this.setState((prevState: NavMenuState) => ({
+      theftDropOpen: !prevState.theftDropOpen,
+    }));
+  };
+
+
   render() {
     if (!this.context) {
       return null;
     }
     const { isLoggedIn, logout } = this.context;
+    const driverDropOpen = this.state.driverDropOpen;
+    const vehicleDropOpen = this.state.vehicleDropOpen;
+    const offenceDropOpen = this.state.offenceDropOpen;
+    const theftDropOpen = this.state.theftDropOpen;
+
 
     return (
       <header>
@@ -35,22 +79,59 @@ export class NavMenu extends Component<object, { collapsed: boolean }> {
           <NavbarBrand tag={Link} to="/">TransportRegister</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           {isLoggedIn && (
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">VehicleList</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/forecast">Forecast</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/link3">Link3</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/login" onClick={logout}>Odhlásit se</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
+            <Collapse className="d-sm-inline-flex navbarDropdownBar" isOpen={!this.state.collapsed} navbar>
+              {/*<div className="navbarDropdownBar">*/}
+              <Dropdown isOpen={driverDropOpen} toggle={this.toggleDriverDropdown} >
+                <DropdownToggle caret color="primary">
+                  Řidiči
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Řidiči</DropdownItem>
+                  <DropdownItem tag={Link} to="/driverSearch">Vyhledat</DropdownItem>
+                  <DropdownItem tag={Link} to="/driverCreate">Zaregistrovat</DropdownItem>
+                  <DropdownItem>?Zobrazit včechny</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+
+              <Dropdown isOpen={vehicleDropOpen} toggle={this.toggleVehicleDropdown}>
+                <DropdownToggle caret color="primary">
+                  Vozidla
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Vozidla</DropdownItem>
+                  <DropdownItem tag={Link} to="/vehicleSearch">Vyhledat</DropdownItem>
+                  <DropdownItem tag={Link} to="/vehicleCreate">-Úředník - Registrovat</DropdownItem>
+                  <DropdownItem>?Zobrazit včechny</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+
+              <Dropdown isOpen={offenceDropOpen} toggle={this.toggleOffenceDropdown}>
+                <DropdownToggle caret color="primary">
+                  Přestupky
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Přestupky</DropdownItem>
+                  <DropdownItem tag={Link} to="/offenceCreate">-Policista - registrovat</DropdownItem>
+                  <DropdownItem tag={Link} to="/offencePending">-Úředník - Zobrazit nevyřešené (své)</DropdownItem>
+                  <DropdownItem>?Vyhledat (asi ne, spíš sobrazit u řidiče TODO)</DropdownItem>
+                  <DropdownItem>?Zobrazit včechny</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+
+              <Dropdown isOpen={theftDropOpen} toggle={this.toggleTheftDropdown}>
+                <DropdownToggle caret color="primary">
+                  Kradená vozidla
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Kradená vozidla</DropdownItem>
+                  <DropdownItem tag={Link} to="/theftCreate">Evidovat krádež</DropdownItem>
+                  <DropdownItem tag={Link} to="/theftFound">-Policista - nahlásit nález</DropdownItem>
+                  <DropdownItem>?Vyhledat</DropdownItem>
+                  <DropdownItem>?Listina/zobrazit vše (v zadání ano, ale má smysl?)</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              {/*</div>*/}
+            </Collapse>
           )}
         </Navbar>
       </header>
