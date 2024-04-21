28ff1bdfedd2e932b7762e01637736bf31057248
xkoren10 <xkoren10@stud.fit.vutbr.cz>
2024-04-21 15:45:22 +0200

    build fix

--- TransportRegister.Server/Repositories/DriverRepository/DriverRepository.cs
+++ TransportRegister.Server/Repositories/DriverRepository/DriverRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace TransportRegister.Server.Repositories.DriverRepository
+﻿using Microsoft.EntityFrameworkCore;
+using TransportRegister.Server.Data;
+using TransportRegister.Server.Models;
+using TransportRegister.Server.Repositories.VehicleRepository;
+
+namespace TransportRegister.Server.Repositories.DriverRepository
 {
-    public class DriverRepository
+    public class DriverRepository : IDriverRepository
     {
+        private readonly AppDbContext _context;
+
+        public DriverRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Driver> GetDriverAsync(string licenseNumber)
+        {
+            return await _context.Drivers
+
+                .FirstOrDefaultAsync(v => v.DriversLicenseNumber == licenseNumber);
+        }
+
     }
 }
