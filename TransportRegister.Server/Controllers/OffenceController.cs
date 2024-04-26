2b9760e688074e5af388dc37befc791d24e92441
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-26 11:00:19 +0200

    Update_Offence_Controller

--- TransportRegister.Server/Controllers/OffenceController.cs
+++ TransportRegister.Server/Controllers/OffenceController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TransportRegister.Server.Data;
+using TransportRegister.Server.DTOs.OffenceDTOs;
 using TransportRegister.Server.Models;
+using TransportRegister.Server.Repositories;
 
 namespace TransportRegister.Server.Controllers
 {
@@ -15,74 +18,99 @@ namespace TransportRegister.Server.Controllers
     public class OffenceController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IOffenceRepository _offenceRepository;
 
-        public OffenceController(AppDbContext context)
+        public OffenceController(AppDbContext context, IOffenceRepository offenceRepository)
         {
             _context = context;
+            _offenceRepository = offenceRepository;
         }
 
-        // GET: api/Offence
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Offence>>> GetOffences()
+        ////////////////// GET METHODS //////////////////
+
+        // GET: api/Offence/5/UnresolvedOffences
+        [HttpGet("{officialId}/UnresolvedOffences")]
+        public async Task<ActionResult<IEnumerable<Offence>>> GetUnresolvedOffences(string officialId)
         {
-            return await _context.Offences.ToListAsync();
+            var offences = await _offenceRepository.GetUnresolvedOfficialsOffencesAsync(officialId);
+
+            if (offences == null)
+            {
+                return NotFound("Nenalezeny žádné přestupky.");
+            }
+
+            return Ok(offences);
         }
 
         // GET: api/Offence/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Offence>> GetOffence(int id)
+        public async Task<ActionResult<OffenceDetailDto>> GetOffence(int id)
         {
-            var offence = await _context.Offences.FindAsync(id);
+            var offence = await _offenceRepository.GetOffenceByIdAsync(id);
 
             if (offence == null)
             {
-                return NotFound();
+                return NotFound("Hledaný přestupek neexistuje.");
             }
 
-            return offence;
+            return Ok(offence);
         }
+
+        ////////////////// POST METHODS //////////////////
+
+        // POST: api/Offence
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("ReportOffence")]
+        [Authorize(Roles = "Officer")]
+        public async Task<ActionResult<int>> ReportOffence(OffenceCreateDto offenceDto)
+        {
+            var offence = await _offenceRepository.ReportOffenceAsync(offenceDto);
+
+            if (offence == null)
+            {
+                return BadRequest("Přestupek se nepodařilo nahlásit.");
+            }
+
+            var assigned = await _offenceRepository.AssignOffenceToOfficialAsync(offence);
+            if (!assigned)
+            {
+                return BadRequest("Přestupek se nepodařilo přiřadit k úředníkovi.");
+            }
+
+            return Ok(offence.OffenceId);
+        }
+
+        ////////////////// PUT METHODS //////////////////
 
         // PUT: api/Offence/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutOffence(int id, Offence offence)
+        public async Task<IActionResult> PutOffence(int id, OffenceCreateDto offence)
         {
-            if (id != offence.OffenceId)
-            {
-                return BadRequest();
-            }
 
-            _context.Entry(offence).State = EntityState.Modified;
+            //_context.Entry(offence).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OffenceExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!OffenceExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
 
-            return NoContent();
+            //return NoContent();
+            return Ok();
         }
 
-        // POST: api/Offence
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Offence>> PostOffence(Offence offence)
-        {
-            _context.Offences.Add(offence);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetOffence", new { id = offence.OffenceId }, offence);
-        }
+        ////////////////// DELETE METHODS //////////////////
 
         // DELETE: api/Offence/5
         [HttpDelete("{id}")]
