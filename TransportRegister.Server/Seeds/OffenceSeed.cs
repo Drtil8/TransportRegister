0ef2b4b05c807c3ff6577d1d9239773bcc4a99c9
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-26 10:58:18 +0200

    Update_OffenceFine_seeds

--- TransportRegister.Server/Seeds/OffenceSeed.cs
+++ TransportRegister.Server/Seeds/OffenceSeed.cs
@@ -8,12 +8,46 @@ namespace TransportRegister.Server.Seeds
         {
             var offencesToSeed = new Offence[]
             {
-                new()
+                new() // Approved offence without fine
                 {
                     ReportedOn = DateTime.Now,
                     IsValid = true,
-                    FineId = null,
-                    VehicleId =1,
+                    IsApproved = true,
+                    //FineId = null,
+                    VehicleId = 1,
+                    OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
+                    OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
+                    PersonId = 2
+                },
+                new() // Yet not approved offence without fine
+                {
+                    ReportedOn = DateTime.Now,
+                    IsValid = true,
+                    IsApproved = false,
+                    //FineId = null,
+                    VehicleId = 1,
+                    OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
+                    OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
+                    PersonId = 2
+                },
+                new() // Denied offence without fine
+                {
+                    ReportedOn = DateTime.Now,
+                    IsValid = false,
+                    IsApproved = false,
+                    //FineId = null,
+                    VehicleId = 1,
+                    OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
+                    OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
+                    PersonId = 2
+                },
+                new() // Approved offence with fine
+                {
+                    ReportedOn = DateTime.Now,
+                    IsValid = true,
+                    IsApproved = true,
+                    //FineId = 1,
+                    VehicleId = 1,
                     OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
                     OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
                     PersonId = 2
