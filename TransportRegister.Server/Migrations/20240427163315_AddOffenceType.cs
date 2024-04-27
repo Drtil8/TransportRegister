576fd2bfc8c081e0035be589c75d352a3163a89d
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-27 19:20:36 +0200

    Update_Migrations_Add_OffenceType

---
+++ TransportRegister.Server/Migrations/20240427163315_AddOffenceType.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportRegister.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOffenceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OffenceTypeId",
+                table: "Offences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "OffenceTypes",
+                columns: table => new
+                {
+                    OffenceTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OffenceTypes", x => x.OffenceTypeId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offences_OffenceTypeId",
+                table: "Offences",
+                column: "OffenceTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Offences_OffenceTypes_OffenceTypeId",
+                table: "Offences",
+                column: "OffenceTypeId",
+                principalTable: "OffenceTypes",
+                principalColumn: "OffenceTypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Offences_OffenceTypes_OffenceTypeId",
+                table: "Offences");
+
+            migrationBuilder.DropTable(
+                name: "OffenceTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Offences_OffenceTypeId",
+                table: "Offences");
+
+            migrationBuilder.DropColumn(
+                name: "OffenceTypeId",
+                table: "Offences");
+        }
+    }
+}
