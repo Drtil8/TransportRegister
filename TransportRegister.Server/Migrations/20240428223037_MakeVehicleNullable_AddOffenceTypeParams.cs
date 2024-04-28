6e81fd317d6170ddb9937cde447d6d7453ebb799
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-29 01:25:05 +0200

    Update_Offence_OffenceType_Models

---
+++ TransportRegister.Server/Migrations/20240428223037_MakeVehicleNullable_AddOffenceTypeParams.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportRegister.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeVehicleNullable_AddOffenceTypeParams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "FineAmount",
+                table: "OffenceTypes",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PenaltyPoints",
+                table: "OffenceTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Offences",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FineAmount",
+                table: "OffenceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "PenaltyPoints",
+                table: "OffenceTypes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Offences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
