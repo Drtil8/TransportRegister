a68fd2d6e09db39a3126e5ce786a992e5bb6d17a
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-29 01:25:42 +0200

    Update_Clients_Add_Interfaces_OffenceDetail_FineDetail

---
+++ transportregister.client/src/components/interfaces/IOffenceDetail.tsx
@@ -0,0 +1,18 @@
+﻿import IFineDetail from "./IFineDetail";
+import IVehicleDetail from "./IVehicleDetail";
+
+interface IOffenceDetail {
+  offenceId: number;
+  reportedOn: Date;
+  //location: string; // todo
+  type: string;
+  isValid: boolean;
+  isApproved: boolean;
+  description: string;
+  vehicle: IVehicleDetail | null; // todo
+  penaltyPoints: number;
+  fine: IFineDetail | null;
+  //photos: string[]; // todo
+}
+
+export default IOffenceDetail;
