9ee351eab17b65e01d626f25761f668531f14471
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-30 22:00:52 +0200

    Implement_UserRepository

---
+++ TransportRegister.Server/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using TransportRegister.Server.DTOs.DatatableDTOs;
+using TransportRegister.Server.DTOs.UserDTOs;
+using TransportRegister.Server.Models;
+
+namespace TransportRegister.Server.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<UserDetailDto> GetUserByIdAsync(string userId);
+        Task<UserDetailDto> GetUserByEmailAsync(string email);
+        Task<bool> UserExistsAsync(string email);
+        IQueryable<UserListItemDto> GetAllUsers();
+        IQueryable<UserListItemDto> ApplyFilterQueryUsers(IQueryable<UserListItemDto> query, DtParamsDto dtParams);
+        Task<TUser> SaveUserAsync<TUser>(UserCreateDto userDto, string roleName) where TUser : User, new();
+        Task EditUserAsync(string userId, UserCreateDto user);
+        Task<bool> RestoreUserAsync(string userId);
+        Task<bool> DeleteUserAsync(string userId);
+    }
+}
