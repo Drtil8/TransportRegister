0e0fbf9315ef75ffcb874662a31e883ebd4aca02
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-30 21:43:45 +0200

    Create_UserDTOs

---
+++ TransportRegister.Server/DTOs/UserDTOs/UserCreateDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransportRegister.Server.DTOs.UserDTOs
+{
+    /// <summary>
+    /// DTO use for creating a new user
+    /// </summary>
+    /// <author> Dominik Pop </author>
+    public class UserCreateDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Role { get; set; }
+        public string PersonalId { get; set; }
+        public string Rank { get; set; }
+    }
+}
---
+++ TransportRegister.Server/DTOs/UserDTOs/UserDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportRegister.Server.DTOs.UserDTOs
+{
+    public class UserDetailDto
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
