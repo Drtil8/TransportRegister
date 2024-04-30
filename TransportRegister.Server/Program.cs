996dc0e98858a7d533c894261f32eea546d48c01
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-30 22:03:25 +0200

    Update_Program_Add_UserRepository

--- TransportRegister.Server/Program.cs
+++ TransportRegister.Server/Program.cs
@@ -38,6 +38,7 @@ namespace TransportRegister.Server
             builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
             builder.Services.AddScoped<ITheftRepository, TheftRepository>();
             builder.Services.AddScoped<IOffenceRepository, OffenceRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
             // todo convertors create unreadable array objects from client side
             // Convertors
