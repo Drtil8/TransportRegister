d6dd52d7cbbe15d171c0144970f9cbf46e98ec1e
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-30 21:42:35 +0200

    Add_Admin_View

---
+++ transportregister.client/src/components/admin/UserCreateModal.tsx
@@ -0,0 +1,148 @@
+﻿import React, { useState } from 'react';
+import { Form, Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, FormGroup, Label, Input, Col } from 'reactstrap';
+
+interface CreateUserModalProps {
+  //fetchDataRef: React.MutableRefObject<any>; // TODO
+}
+
+const UserCreateModal: React.FC<CreateUserModalProps> = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const password = (document.getElementById("createUserPassword") as HTMLInputElement).value;
+    const passwordAgain = (document.getElementById("createUserPasswordAgain") as HTMLInputElement).value;
+    if (password !== passwordAgain) {
+      const error = document.getElementById("error");
+      error!.classList.remove("hidden");
+      error!.innerText = "* Hesla se neshodují";
+      return;
+    }
+
+    // TODO -> password encryption when sending
+    try {
+      const response = await fetch('/api/User', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: (document.getElementById("createUserEmail") as HTMLInputElement).value,
+          firstname: (document.getElementById("createUserFirstname") as HTMLInputElement).value,
+          lastname: (document.getElementById("createUserLastname") as HTMLInputElement).value,
+          role: (document.getElementById("createUserRole") as HTMLInputElement).value,
+          personalNumber: (document.getElementById("createUserPersonalNumber") as HTMLInputElement).value,
+          rank: (document.getElementById("createUserRank") as HTMLInputElement).value,
+          password: (document.getElementById("createUserPassword") as HTMLInputElement).value,
+          passwordConfirm: (document.getElementById("createUserPasswordAgain") as HTMLInputElement).value
+        })
+      });
+
+      if (response.ok) {
+        //const fetchData = fetchDataRef.current;
+        //fetchData();
+        toggle();
+      }
+      else {
+        const error = document.getElementById("error");
+        error!.classList.remove("hidden");
+        error!.innerText = await response.text();
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    const officerRow = document.getElementById("createUserOfficerRow");
+    if (value === "officer") {
+      officerRow?.classList.remove("hidden");
+    }
+    else {
+      officerRow?.classList.add("hidden");
+    }
+  }
+
+  return (
+    <div>
+      <Button color="success" onClick={toggle}>
+        Přidat uživatele
+      </Button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Tvorba uživatele</ModalHeader>
+        <Form id="createUserForm" onSubmit={handleSubmit}>
+          <ModalBody>
+            <FormGroup>
+              <Row>
+                <Col>
+                  <Label> Email:</Label>
+                  <Input required id="createUserEmail" name="createUserEmail" type="email" />
+                </Col>
+              </Row>
+              <Row className="mt-2">
+                <Col>
+                  <Label> Jméno:</Label>
+                  <Input required id="createUserFirstname" name="createUserFirstname" type="text" />
+                </Col>
+                <Col>
+                  <Label> Příjmení:</Label>
+                  <Input required id="createUserLastname" name="createUserLastname" type="text" />
+                </Col>
+              </Row>
+              <Row className="mt-2">
+                <Col>
+                  <Label> Typ účtu:</Label>
+                  <Input required id="createUserRole" name="createUserRole" type="select" onChange={handleSelectChange}>
+                    <option value="Official">Úředník</option>
+                    <option value="Officer">Policista</option>
+                    <option value="Admin">Administrátor</option>
+                  </Input>
+                </Col>
+              </Row>
+              <Row id="createUserOfficerRow" className="hidden mt-2">
+                <Col>
+                  <Label> Osobní číslo:</Label>
+                  <Input id="createUserPersonalNumber" name="createUserPersonalNumber" type="text" />
+                </Col>
+                <Col>
+                  <Label>Hodnost</Label>
+                  <Input id="createUserRank" name="createUserRank" type="text" />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Label>Heslo:</Label>
+                  <Input id="createUserPassword" name="createUserPassword" type="password"></Input>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Label>Heslo znovu:</Label>
+                  <Input id="createUserPasswordAgain" name="createUserPasswordAgain" type="password"></Input>
+                </Col>
+              </Row>
+              <Row>
+                <p id="error" className="text-danger hidden"></p>
+              </Row>
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="success" type="submit">
+              Vytvořit
+            </Button>
+            <Button color="secondary" onClick={toggle}>
+              Zrušit
+            </Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
+    </div>
+  );
+}
+
+export default UserCreateModal;
---
+++ transportregister.client/src/components/admin/UserDeleteModal.tsx
@@ -0,0 +1,59 @@
+﻿import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Alert } from 'reactstrap';
+
+interface IUserDeleteModalProps {
+  userId: string | null;
+  isOpen: boolean;
+  toggle: () => void;
+  fetchData: () => void;
+}
+
+
+const UserDeleteModal: React.FC<IUserDeleteModalProps> = ({ userId, isOpen, toggle, fetchData }) => {
+
+  const delteUser = async () => {
+    try {
+      const response = await fetch(`api/User/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        fetchData(); // Fetch data again
+        toggle();
+      }
+      else {
+        console.error(response.statusText);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  return (
+    <Modal isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Vymazání projektu</ModalHeader>
+      <ModalBody>
+        <Alert className="alert alert-warning">
+          Opravdu chcete vymazat uživatele?
+          Uživatelský účet bude znevalidněn a nebude možné se s ním přihlásit.
+          Tato operace jde vrátit.
+        </Alert>
+      </ModalBody>
+      <ModalFooter className="d-flex align-items-center justify-content-center">
+        <Button color="danger" onClick={delteUser}>
+          Vymazat
+        </Button>
+        <Button color="secondary" onClick={toggle}>
+          Zrušit
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+}
+
+export default UserDeleteModal;
