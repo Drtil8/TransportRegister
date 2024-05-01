5e372ae22cba63b0dc66bb7ae63bc0d2fe049e36
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-01 19:14:04 +0200

    Update_DTOs

--- TransportRegister.Server/DTOs/UserDTOs/UserDetailDto.cs
+++ TransportRegister.Server/DTOs/UserDTOs/UserDetailDto.cs
@@ -2,10 +2,13 @@
 {
     public class UserDetailDto
     {
-        public int UserId { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
+        public bool IsValid { get; set; }
+        public int? PersonalId { get; set; }
+        public string Rank{ get; set;}
     }
 }
---
+++ TransportRegister.Server/DTOs/UserDTOs/UserSimpleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TransportRegister.Server.DTOs.UserDTOs
+{
+    public class UserSimpleDto
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
