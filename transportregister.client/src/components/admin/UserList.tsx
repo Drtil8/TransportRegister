686e383a48d2a73d7b99d7e93561820a56fa869b
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-01 15:11:16 +0200

    Update_UserDatatables

--- transportregister.client/src/components/admin/UserList.tsx
+++ transportregister.client/src/components/admin/UserList.tsx
@@ -1,22 +1,32 @@
-﻿import { Component } from 'react';
+﻿import { Component, ContextType } from 'react';
 import { Col, Row } from 'reactstrap';
 import UserCreateModal from './UserCreateModal';
 import UserDatatable from './UserDatatable';
+import AuthContext from '../../auth/AuthContext';
+import { Navigate } from 'react-router-dom';
+import IDtFetchData from '../interfaces/datatables/IDtFetchData';
+
 export class UserList extends Component<object> {
   fetchDataRef: React.MutableRefObject<IDtFetchData | null> = { current: null };
+  static contextType = AuthContext;
+  declare context: ContextType<typeof AuthContext>;
   constructor(props: object) {
     super(props);
   }
 
   render() {
+    if (!this.context?.isAdmin) {
+      return <Navigate to="/" replace />;
+    }
+
     return (
       <>
-        <Row>
+        <Row className="mb-3">
           <Col>
           <h4>Uživatelé</h4>
           </Col>
           <Col className="d-flex justify-content-end">
-          <UserCreateModal />
+            <UserCreateModal fetchDataRef={this.fetchDataRef} />
           </Col>
         </Row>
         <UserDatatable fetchDataRef={this.fetchDataRef} />
