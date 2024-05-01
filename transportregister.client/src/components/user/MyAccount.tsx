5bea486187d2abd85f95aba785535f927e45ea8d
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-01 19:12:33 +0200

    Add_Client_User_components

---
+++ transportregister.client/src/components/user/MyAccount.tsx
@@ -0,0 +1,26 @@
+﻿import { Component } from "react";
+import { Col, Row } from "reactstrap";
+
+export class MyAccount extends Component<object> {
+  constructor(props: object) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Můj účet</h1>
+        <Row>
+          <Col>
+            email, jmeno, role, ...
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            moznost zmenit heslo
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
---
+++ transportregister.client/src/components/user/UserDetail.tsx
@@ -0,0 +1,99 @@
+﻿import { Component } from "react";
+import { Alert, Col, Row } from "reactstrap";
+import IUserDetail from "../interfaces/IUserDetail";
+
+interface UserDetailProps {
+  userDetail: IUserDetail | null;
+}
+
+export class UserDetail extends Component<object, UserDetailProps> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      userDetail: null
+    }
+  }
+
+  componentDidMount() {
+    this.populateUserData();
+  }
+
+  async populateUserData() {
+    const urlSplitted = window.location.pathname.split('/');
+    const id = urlSplitted[2];
+    const apiUrl = `/api/User/${id}`;
+
+    try {
+      const response = await fetch(apiUrl);
+
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({ userDetail: data });
+      }
+      else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    const { userDetail } = this.state;
+
+    return (
+      <div>
+        {this.state.userDetail === null ? (
+          <Alert color="danger"> Uživatel nenalezen!</Alert>
+        )
+          :
+          (
+            <>
+              <h1>Detail uživatele</h1>
+              <Row>
+                <Col>
+                  <dl>
+                    <Row>
+                      <Col>
+                        <dt>Celé jméno:</dt>
+                        <dd>{userDetail?.firstName} {userDetail?.lastName}</dd>
+                      </Col>
+                      <Col>
+                        <dt>Email:</dt>
+                        <dd>{userDetail?.email}</dd>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <dt>Role:</dt>
+                        <dd>{userDetail?.role}</dd>
+                      </Col>
+                      <Col>
+                        <dt>Stav účtu:</dt>
+                        <dd>{userDetail?.isValid ? "Aktivní" : "Deaktivovaný"}</dd>
+                      </Col>
+                    </Row>
+                    {userDetail?.role === "Policista" && (
+                      <Row>
+                        <Col>
+                          <dt>Služební číslo:</dt>
+                          <dd>{userDetail.personalId}</dd>
+                        </Col>
+                        <Col>
+                          <dt>Hodnost:</dt>
+                          <dd>{userDetail.rank}</dd>
+                        </Col>
+                      </Row>
+                    )}
+                  </dl>
+                </Col>
+              </Row>
+            </>
+
+          )
+        }
+      </div>
+    );
+  }
+}
