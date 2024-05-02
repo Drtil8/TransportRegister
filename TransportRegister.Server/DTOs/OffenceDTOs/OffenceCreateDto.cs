622595dc619365e8372bbfb0546ae213722ded13
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-02 13:13:44 +0200

    Update_OffenceCreateDto_Add_Photos

--- TransportRegister.Server/DTOs/OffenceDTOs/OffenceCreateDto.cs
+++ TransportRegister.Server/DTOs/OffenceDTOs/OffenceCreateDto.cs
@@ -9,12 +9,13 @@ namespace TransportRegister.Server.DTOs.OffenceDTOs
     public class OffenceCreateDto
     {
         public string Description { get; set; }
-        public int OffenceTypeId { get; set; } // TODO
+        public int OffenceTypeId { get; set; }
         public int PenaltyPoints { get; set; }
         public Address Address { get; set; } // TODO
         public double FineAmount { get; set; }
         public bool FinePaid { get; set; }
         public int VehicleId { get; set; } // Comitted on vehicle
         public int PersonId { get; set; } // Comitted by person -> if created for driver, then it is driver, if created for vechicle then it is owner
+        public ICollection<string> Photos { get; set; }
     }
 }
