d38b9a9b06e489d3a430a5b885fa396e40dc40e1
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-02 02:03:10 +0200

    Update_Seeds_OffencePhotos

---
+++ TransportRegister.Server/Seeds/OffencePhotoSeed.cs
@@ -0,0 +1,34 @@
+﻿using TransportRegister.Server.Data;
+using TransportRegister.Server.Models;
+
+namespace TransportRegister.Server.Seeds
+{
+    public class OffencePhotoSeed
+    {
+        public static void Seed(AppDbContext context)
+        {
+            var photosToSeed = new OffencePhoto[]
+            {
+                new()
+                {
+                    Image = File.ReadAllBytes("Seeds/Images/Offences/istockphoto-471089928-612x612.jpg"),
+                    OffenceId = 2
+                },
+                new()
+                {
+                    Image = File.ReadAllBytes("Seeds/Images/Offences/prestupek-parkovani-praha-policie.jpg"),
+                    OffenceId = 2
+                }
+            };
+
+            foreach (var photo in photosToSeed)
+            {
+                if (!context.OffencePhotos.Any(p => p.OffencePhotoId == photo.OffencePhotoId))
+                {
+                    context.OffencePhotos.Add(photo);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- TransportRegister.Server/Seeds/OffenceSeed.cs
+++ TransportRegister.Server/Seeds/OffenceSeed.cs
@@ -8,48 +8,44 @@ namespace TransportRegister.Server.Seeds
         {
             var offencesToSeed = new Offence[]
             {
-                new() // Approved offence without fine
+                new() // Approved offence with fine
                 {
-                    ReportedOn = DateTime.Now,
+                    ReportedOn = DateTime.Parse("2024-04-07 12:00:00"),
                     IsValid = true,
                     IsApproved = true,
-                    //FineId = null,
                     VehicleId = 1,
                     OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
                     OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
                     PersonId = 2,
                     OffenceTypeId = 2
                 },
-                new() // Yet not approved offence without fine
+                new() // Yet not approved offence with fine
                 {
-                    ReportedOn = DateTime.Now,
+                    ReportedOn = DateTime.Parse("2024-05-04 12:00:00"),
                     IsValid = true,
                     IsApproved = false,
-                    //FineId = null,
                     VehicleId = 1,
                     OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
                     //OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
                     PersonId = 2,
-                    OffenceTypeId = 3
+                    OffenceTypeId = 10
                 },
-                new() // Denied offence without fine
+                new() // Denied offence with fine
                 {
-                    ReportedOn = DateTime.Now,
+                    ReportedOn = DateTime.Parse("2024-05-01 12:00:00"),
                     IsValid = false,
                     IsApproved = false,
-                    //FineId = null,
                     VehicleId = 1,
                     OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
                     OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
                     PersonId = 2,
                     OffenceTypeId = 1
                 },
-                new() // Approved offence with fine
+                new() // Approved offence without fine
                 {
                     ReportedOn = DateTime.Now,
                     IsValid = true,
                     IsApproved = true,
-                    //FineId = 1,
                     VehicleId = 1,
                     OfficerId = "39123a3c-3ce3-4bcc-8887-eb7d8e975ea8",
                     OfficialId = "d6f46418-2c21-43f8-b167-162fb5e3a999",
