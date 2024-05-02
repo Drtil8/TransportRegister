0a157e20dbfec03459c435ac32ce728f8daa89e7
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-05-02 02:04:44 +0200

    Update_Migrations

---
+++ TransportRegister.Server/Migrations/20240501215226_AddOffencePhotos.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportRegister.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOffencePhotos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OffencePhotos",
+                columns: table => new
+                {
+                    OffencePhotoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    OffenceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OffencePhotos", x => x.OffencePhotoId);
+                    table.ForeignKey(
+                        name: "FK_OffencePhotos_Offences_OffenceId",
+                        column: x => x.OffenceId,
+                        principalTable: "Offences",
+                        principalColumn: "OffenceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OffencePhotos_OffenceId",
+                table: "OffencePhotos",
+                column: "OffenceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OffencePhotos");
+        }
+    }
+}
