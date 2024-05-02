3076851e1246f7fd3c59157072ebf14b9ea27337
xhosad00 <80786156+xhosad00@users.noreply.github.com>
2024-05-02 21:21:54 +0200

    Driver_licenses_form

--- transportregister.client/src/components/interfaces/IDriverForm.tsx
+++ transportregister.client/src/components/interfaces/IDriverForm.tsx
@@ -1,30 +1,12 @@
-﻿
-
-interface IDriverFormState {
-  firstName: string;
-  lastName: string;
-  birthNumber1: number; // before slash
-  birthNumber2: number; // after slash
-  sex_Male: boolean;
-  dateOfBirth: string;
-  //address: {
-  //  street: string;
-  //  city: string;
-  //  state: string;
-  //  country: string;
-  //  houseNumber: string;
-  //  postalCode: string;
-  //};
-  image: string; //TODO CHANGE TYPE
-  //officialId: string;
+﻿interface IDriverFormState {
   driversLicenseNumber: string;
   badPoints: number;
   hasSuspendedLicense: boolean;
   lastCrimeCommited: string;
   drivingSuspendedUntil: string;
-  licenses: any[]; // Adjust this type based on your `DriversLicense` interface
 
   disableInput: boolean;
+  licensesStrings: string[];
 }
 
 export default IDriverFormState;
